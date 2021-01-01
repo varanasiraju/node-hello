7d9d5d7f977c6052ccbc883232fcc3aa43205adf
varanasiraju <46042371+varanasiraju@users.noreply.github.com>
2021-01-01 20:13:40 +0530

    update msg 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'welcome to delixus!\n'
+  const msg = 'welcome to delixus @ india @ \n'
   res.end(msg);
 });
 
